ecaeec343727aec12aed4fad73e8bcb5f1725590
MATALAME <141719513+MATALAME@users.noreply.github.com>
2025-06-03 17:31:04 +0200

    Hamburger

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { useState } from "react";
 import './NavBarStyle.css';
 import logo from "../Images/RawGamesLogoWhite.png";
-import "../App.css"
+
+import hamburger from "../Images/Hamburger.png";
 
 function NavBar() {
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const hideNavbarPaths = ['/', '/signup'];
 
   if (hideNavbarPaths.includes(location.pathname.toLowerCase())) { // Doesn't render the nav bar if the app is on one these paths
     return null;
   }
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <nav className="navbar">
       <img src={logo} alt="Raw Games" className="navbar-logo" />
-      <div className="nav-links-container">
-        <NavLink to="/home" className="nav-link" viewTransition >Home</NavLink>
-        <NavLink to="/games" className="nav-link"viewTransition>Games</NavLink>
-        <NavLink to="/deals" className="nav-link"viewTransition>Deals</NavLink>
-        <NavLink to="/profile" className="nav-link"viewTransition>Profile</NavLink>
+      <img src={hamburger} className="hamburger" onClick={toggleMenu}/>  
+
+      <div className={`nav-links-container ${isMenuOpen ? 'show' : ''}`}>
+        <NavLink to="/home" className="nav-link" onClick={() => setIsMenuOpen(false)}>Home</NavLink>
+        <NavLink to="/games" className="nav-link" onClick={() => setIsMenuOpen(false)}>Games</NavLink>
+        <NavLink to="/deals" className="nav-link" onClick={() => setIsMenuOpen(false)}>Deals</NavLink>
+        <NavLink to="/profile" className="nav-link" onClick={() => setIsMenuOpen(false)}>Profile</NavLink>
       </div>
     </nav>
   );
